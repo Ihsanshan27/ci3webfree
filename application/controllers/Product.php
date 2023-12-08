637696899085e69f361994efa77c760691ce30f2
Muhammad Ihsan <ihsanshan5@gmail.com>
2023-12-08 21:09:23 +0700

    menambahkan asset gambar

---
+++ application/controllers/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Product extends MY_Controller
+{
+
+
+     public function __construct()
+     {
+          parent::__construct();
+
+     }
+
+
+     public function index()
+     {
+
+     }
+
+}
+
+/* End of file Product.php */
+
+
+?>
